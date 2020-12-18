a3d656e8de31cda320562fd09833d1a0f67b9d61
Juancho <juang77@hotmail.com>
2020-12-18 12:20:21 -0500

    Eliminar Tweet - 66

---
+++ routers/eliminarTweet.go
@@ -0,0 +1,23 @@
+package routers
+
+import (
+	"net/http"
+
+	"github.com/juang77/Gabotor/bd"
+)
+
+//EliminarTweet is la funcion que permite eliminar un Tweet por su ID
+func EliminarTweet(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parámetro ID", http.StatusBadRequest)
+		return
+	}
+	err := bd.BorroTweet(ID, IDUsuario)
+	if err != nil {
+		http.Error(w, "Ocurrió un error al intentar corrar el tweet"+err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+}
