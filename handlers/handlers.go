2cb8b96da177bc226a65f2ebc73ca776a93b2da3
Juancho <juang77@hotmail.com>
2020-12-21 16:33:05 -0500

    Traer Usuario- 81

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -32,6 +32,8 @@ func Manejadores() {
 	router.HandleFunc("/bajaRelacion", middlew.ChequeoDB(middlew.ValidoJWT(routers.BajaRelacion))).Methods("DELETE")
 	router.HandleFunc("/consultaRelacion", middlew.ChequeoDB(middlew.ValidoJWT(routers.ConsultaRelacion))).Methods("GET")
 
+	router.HandleFunc("/listaUsuarios", middlew.ChequeoDB(middlew.ValidoJWT(routers.ListaUsuarios))).Methods("GET")
+
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
